b7a964d8802fa51aa630ce714a7e708267c7fe58
Léo <legrivel@student.le-101.fr>
2019-01-05 03:17:04 +0100

    Modified criteria

--- server/api/websites/bienici.api.js
+++ server/api/websites/bienici.api.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 module.exports = {
 	getHome: () => {
-			return fetch("https://www.bienici.com/realEstateAds.json?filters={\"size\":24,\"from\":0,\"filterType\":\"rent\",\"propertyType\":[\"house\",\"flat\"],\"maxPrice\":1250,\"minRooms\":4,\"newProperty\":false,\"page\":1,\"resultsPerPage\":24,\"maxAuthorizedResults\":2400,\"sortBy\":\"relevance\",\"sortOrder\":\"desc\",\"onTheMarket\":[true],\"mapMode\":\"enabled\",\"limit\":\"cpowGynz[?_vh@jxmArC?vlh@\",\"showAllModels\":false,\"blurInfoType\":[\"disk\",\"exact\"],\"zoneIdsByTypes\":{\"zoneIds\":[\"-120965\"]}}")
+			return fetch("https://www.bienici.com/realEstateAds.json?filters={\"size\":24,\"from\":0,\"filterType\":\"rent\",\"propertyType\":[\"house\",\"flat\"],\"maxPrice\":1250,\"minRooms\":4,\"newProperty\":false,\"page\":1,\"resultsPerPage\":24,\"maxAuthorizedResults\":2400,\"sortBy\":\"relevance\",\"sortOrder\":\"desc\",\"onTheMarket\":[true],\"mapMode\":\"enabled\",\"limit\":\"cpowGynz[?_vh@jxmArC?vlh@\",\"showAllModels\":false,\"blurInfoType\":[\"disk\",\"exact\"],\"zoneIdsByTypes\":{\"zoneIds\":[\"-10680\", \"-10682\"]}}")
 				.then(result => {
 					return result.json()
 						.then(result => result.realEstateAds.map(home => ({
